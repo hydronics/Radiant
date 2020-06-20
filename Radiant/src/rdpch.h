5d5c9b4b39d7af5fde0eefcb773662aa5e503af5
Daniel Strycharske <hydronics311@gmail.com>
2020-06-20 13:20:38 -0700

    Adding the actual precompiled header and source file.

---
+++ Radiant/src/rdpch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+
+#ifdef RD_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
